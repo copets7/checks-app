60912ccdef96ee4c48d6d8a4a76580712922a1c2
Andrey Yarosh <andreyarosh7@gmail.com>
2023-01-21 15:19:05 +0300

    ref: Optional wrapper added

--- src/main/java/com/yarosh/checks/repository/CrudRepository.java
+++ src/main/java/com/yarosh/checks/repository/CrudRepository.java
@@ -1,11 +1,12 @@
 package com.yarosh.checks.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CrudRepository<E, ID> {
 
     E insert(E entity);
-    E find(E entity);
+    Optional<E> find(ID id);
     List<E> findAll();
     E update(E entity);
     void delete(ID id);
--- src/main/java/com/yarosh/checks/service/CrudService.java
+++ src/main/java/com/yarosh/checks/service/CrudService.java
@@ -1,10 +1,11 @@
 package com.yarosh.checks.service;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CrudService<D, ID> {
     D add(D domain);
-    D get(D domain);
+    Optional<D> get(ID id);
     List<D> getAll();
     D update(D domain);
     void delete(ID id);
