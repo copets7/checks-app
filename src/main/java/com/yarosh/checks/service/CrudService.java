cba712869a9a022e898fe4221d9e80e7ca8b82e1
Andrey Yarosh <andreyarosh7@gmail.com>
2023-02-04 15:58:24 +0300

    feature : interface CrudeService class D extends Domain

--- src/main/java/com/yarosh/checks/service/CrudService.java
+++ src/main/java/com/yarosh/checks/service/CrudService.java
@@ -1,13 +1,19 @@
 package com.yarosh.checks.service;
 
+import com.yarosh.checks.domain.Domain;
+
 import java.util.List;
 import java.util.Optional;
 
-public interface CrudService<D, ID> {
-    D add(D domain);
-    Optional<D> get(ID id);
-    List<D> getAll();
-    D update(D domain);
-    void delete(ID id);
+public interface CrudService<D extends Domain, ID> {
 
+    D add(D domain);
+
+    Optional<D> get(ID id);
+
+    List<D> getAll();
+
+    D update(D domain);
+
+    void delete(ID id);
 }
