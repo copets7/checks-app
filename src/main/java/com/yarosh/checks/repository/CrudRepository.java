5d42662de968cdb7290d4ba86b306077c344deeb
Andrey Yarosh <andreyarosh7@gmail.com>
2023-02-04 15:59:17 +0300

    feature : interface CrudRepository class E extends Entity

--- src/main/java/com/yarosh/checks/repository/CrudRepository.java
+++ src/main/java/com/yarosh/checks/repository/CrudRepository.java
@@ -1,13 +1,19 @@
 package com.yarosh.checks.repository;
 
+import com.yarosh.checks.repository.entity.Entity;
+
 import java.util.List;
 import java.util.Optional;
 
-public interface CrudRepository<E, ID> {
+public interface CrudRepository<E extends Entity, ID> {
 
     E insert(E entity);
+
     Optional<E> find(ID id);
+
     List<E> findAll();
+
     E update(E entity);
+
     void delete(ID id);
 }
