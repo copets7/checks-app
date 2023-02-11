934aba53f2712ff0184455b1025482b9df53ed37
Andrey Yarosh <andreyarosh7@gmail.com>
2023-02-11 15:31:30 +0300

    feature: created method performProducts in CheckServiceImpl.java

--- src/main/java/com/yarosh/checks/service/check/CheckServiceImpl.java
+++ src/main/java/com/yarosh/checks/service/check/CheckServiceImpl.java
@@ -1,7 +1,8 @@
 package com.yarosh.checks.service.check;
 
 import com.yarosh.checks.domain.Check;
-import com.yarosh.checks.domain.Product;
+import com.yarosh.checks.domain.DiscountCard;
+import com.yarosh.checks.domain.product.Product;
 import com.yarosh.checks.repository.CrudRepository;
 import com.yarosh.checks.repository.entity.CheckEntity;
 import com.yarosh.checks.service.CrudService;
@@ -15,19 +16,26 @@ import java.util.function.Function;
 public class CheckServiceImpl implements CheckService {
 
     private final CrudRepository<CheckEntity, Long> checkRepository;
+
     private final CrudService<Product, Long> productService;
+    private final CrudService<DiscountCard, Long> discountCardService;
+
     private final BidirectionalConverter<Check, CheckEntity> checkConverter;
 
     public CheckServiceImpl(CrudRepository<CheckEntity, Long> checkRepository,
                             CrudService<Product, Long> productService,
+                            CrudService<DiscountCard, Long> discountCardService,
                             BidirectionalConverter<Check, CheckEntity> checkConverter) {
         this.checkRepository = checkRepository;
         this.productService = productService;
+        this.discountCardService = discountCardService;
         this.checkConverter = checkConverter;
     }
 
     @Override
-    public Check performCheck(Map<Long, Integer> productsQuantity, Long discountCardId) {
+    public Check performCheck(Long discountCardId, Map<Long, Integer> productsQuantity) {
+        Optional<DiscountCard> maybeDiscountCard = discountCardService.get(discountCardId);
+
         return null;
     }
 
@@ -65,5 +73,16 @@ public class CheckServiceImpl implements CheckService {
         return checkConverter.convertToDomain(upsertedCheck);
     }
 
+    private List<Product> performProducts(Map<Long, Integer> productsQuantity) {
+        return productsQuantity.entrySet()
+                .stream()
+                .map(entry -> performProduct(entry.getKey(), entry.getValue()))
+                .toList();
+    }
 
+    private Product performProduct(long id, int quantity) {
+        return productService.get(id)
+                .map(product -> product.performProductForCheck(quantity))
+                .orElseThrow(() -> new ProductNotFoundException(id));
+    }
 }
