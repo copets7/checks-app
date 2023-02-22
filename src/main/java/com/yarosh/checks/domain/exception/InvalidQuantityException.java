39e1a5d56137230d043ec63c6b0ab16ad56aba62
Andrey Yarosh <andreyarosh7@gmail.com>
2023-02-22 20:49:00 +0300

    ref: change int quantity to Integer

--- src/main/java/com/yarosh/checks/domain/exception/InvalidQuantityException.java
+++ src/main/java/com/yarosh/checks/domain/exception/InvalidQuantityException.java
@@ -1,11 +1,11 @@
-package com.yarosh.checks.domain.product;
+package com.yarosh.checks.domain.exception;
 
 import java.text.MessageFormat;
 
 public class InvalidQuantityException extends RuntimeException {
 
-    public InvalidQuantityException(long id, int quantity) {
-        this("Quantity of product is less than 1, id: {0}, quantity: {1}", id, quantity);
+    public InvalidQuantityException(long id, Integer quantity) {
+        this("Quantity of product is less than 1 or null, id: {0}, quantity: {1}", id, quantity);
     }
 
     public InvalidQuantityException(String templateMessage, Object... params) {
