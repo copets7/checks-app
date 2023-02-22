ebf150eaff8f3303930e68374a7155aeb7741cfb
Andrey Yarosh <andreyarosh7@gmail.com>
2023-02-22 20:50:57 +0300

    feature: added methods getValidatedQuantity and performProductForCheck

--- src/main/java/com/yarosh/checks/domain/Product.java
+++ src/main/java/com/yarosh/checks/domain/Product.java
@@ -1,28 +1,43 @@
-package com.yarosh.checks.domain.product;
+package com.yarosh.checks.domain;
 
-import com.yarosh.checks.domain.Domain;
+import com.yarosh.checks.domain.exception.InvalidQuantityException;
 
 import java.util.Objects;
 import java.util.Optional;
 
 public class Product implements Domain {
 
+    private static final int INVALID_QUANTITY = 0;
+
     private final Long id;
     private final String description;
     private final Optional<Integer> quantityInCheck;
-    private final int price;
+    private final double price;
     private final double discount;
 
     public Product(Long id,
                    String description,
                    Optional<Integer> quantityInCheck,
-                   int price,
+                   double price,
                    double discount) {
         this.id = id;
         this.description = description;
         this.quantityInCheck = quantityInCheck;
         this.price = price;
         this.discount = discount;
+    }
+
+    public int getValidatedQuantity() {
+        return getQuantityInCheck().filter(quantity -> quantity > INVALID_QUANTITY)
+                .orElseThrow(() -> new InvalidQuantityException(getId(), getQuantityInCheck().get()));
+    }
+
+    public Product performProductForCheck(int quantityInCheck) {
+        if (quantityInCheck <= INVALID_QUANTITY) {
+            throw new InvalidQuantityException(id, quantityInCheck);
+        }
+
+        return new Product(id, description, Optional.of(quantityInCheck), price, discount);
     }
 
     public Long getId() {
@@ -37,15 +52,7 @@ public class Product implements Domain {
         return quantityInCheck;
     }
 
-    public Product performProductForCheck(int quantityInCheck) {
-        if (quantityInCheck <= 0) {
-            throw new InvalidQuantityException(id, quantityInCheck);
-        }
-
-        return new Product(id, description, Optional.of(quantityInCheck), price, discount);
-    }
-
-    public int getPrice() {
+    public double getPrice() {
         return price;
     }
 
