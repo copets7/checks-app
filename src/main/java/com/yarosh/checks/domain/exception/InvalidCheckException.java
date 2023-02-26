e38d582aee6931ce562ef8cfee8522a3890c55d9
Andrey Yarosh <andreyarosh7@gmail.com>
2023-02-26 18:43:23 +0300

    ref: renamed class InvalidTotalPriceException.java to InvalidCheckException.java

--- src/main/java/com/yarosh/checks/domain/exception/InvalidCheckException.java
+++ src/main/java/com/yarosh/checks/domain/exception/InvalidCheckException.java
@@ -2,13 +2,13 @@ package com.yarosh.checks.domain.exception;
 
 import java.text.MessageFormat;
 
-public class InvalidTotalPriceException extends RuntimeException {
+public class InvalidCheckException extends RuntimeException {
 
-    public InvalidTotalPriceException(Double totalPrice) {
+    public InvalidCheckException(Double totalPrice) {
         this("Total price in check is less than 0.1 or null, price: {0}", totalPrice);
     }
 
-    public InvalidTotalPriceException(String templateMessage, Object... params) {
+    public InvalidCheckException(String templateMessage, Object... params) {
         super(MessageFormat.format(templateMessage, params));
     }
 }
