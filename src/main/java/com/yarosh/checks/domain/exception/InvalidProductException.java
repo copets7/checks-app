62ebe27f996cb1db44f8c823dcc3c1c9d7d04d47
Andrey Yarosh <andreyarosh7@gmail.com>
2023-02-26 18:43:53 +0300

    feature: added class InvalidProductException.java

---
+++ src/main/java/com/yarosh/checks/domain/exception/InvalidProductException.java
@@ -0,0 +1,14 @@
+package com.yarosh.checks.domain.exception;
+
+import java.text.MessageFormat;
+
+public class InvalidProductException extends RuntimeException {
+
+    public InvalidProductException(String templateMessage, Object... params) {
+        super(MessageFormat.format(templateMessage, params));
+    }
+
+    public static InvalidProductException quantityCase(long id, Integer quantity) {
+        return new InvalidProductException("Quantity of product is less than 1 or null, id: {0}, quantity: {1}", id, quantity);
+    }
+}
