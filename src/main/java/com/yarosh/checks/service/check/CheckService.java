aa0cd9c48c0ed6b048f5e328ae3ca005ca17fe69
Andrey Yarosh <andreyarosh7@gmail.com>
2023-03-01 20:36:38 +0300

    ref: change CrudService<Check, Long> to CrudService<Check, CheckId> and performCheck(Long discountCardId, Map<Long, Integer> productsQuantity) to performCheck(DiscountCardId discountCardId, Map<ProductId, Integer> productsQuantity)

--- src/main/java/com/yarosh/checks/service/check/CheckService.java
+++ src/main/java/com/yarosh/checks/service/check/CheckService.java
@@ -1,11 +1,14 @@
 package com.yarosh.checks.service.check;
 
 import com.yarosh.checks.domain.Check;
+import com.yarosh.checks.domain.id.CheckId;
+import com.yarosh.checks.domain.id.DiscountCardId;
+import com.yarosh.checks.domain.id.ProductId;
 import com.yarosh.checks.service.CrudService;
 
 import java.util.Map;
 
-public interface CheckService extends CrudService<Check, Long> {
+public interface CheckService extends CrudService<Check, CheckId> {
 
-    Check performCheck(Long discountCardId, Map<Long, Integer> productsQuantity);
+    Check performCheck(DiscountCardId discountCardId, Map<ProductId, Integer> productsQuantity);
 }
