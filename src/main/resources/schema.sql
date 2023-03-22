0371ccd6350a1216acc43fcfcdbbae13205b612e
Andrey Yarosh <andreyarosh7@gmail.com>
2023-03-22 21:17:00 +0300

    feature: added sql script to create tables

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -10,13 +10,14 @@ CREATE TABLE IF NOT EXISTS products(
     discount DOUBLE NOT NULL
 );
 
-CREATE TABLE IF NOT EXIST checks(
+CREATE TABLE IF NOT EXISTS checks(
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     market_name VARCHAR(256) NOT NULL,
     cashier_name VARCHAR(256) NOT NULL,
     date DATE NOT NULL,
     time TIME NOT NULL,
+    products JSON NOT NULL,
     discount_card_id BIGINT NOT NULL,
     total_price DOUBLE NOT NULL,
-    FOREIGN KEY(discount_cards_id) REFERENCES discount_cards (id)
+    FOREIGN KEY(discount_card_id) REFERENCES discount_cards (id)
 );
