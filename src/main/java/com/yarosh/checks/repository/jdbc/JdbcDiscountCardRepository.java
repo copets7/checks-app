7edf1ec5289e06a7a418791ff8754d15ebcf9579
Andrey Yarosh <andreyarosh7@gmail.com>
2023-03-25 15:57:43 +0300

    ref: changed DiscountCardRepository to JdbcDiscountCardRepository feature: added fields and finished method findAll

--- src/main/java/com/yarosh/checks/repository/jdbc/JdbcDiscountCardRepository.java
+++ src/main/java/com/yarosh/checks/repository/jdbc/JdbcDiscountCardRepository.java
@@ -5,20 +5,23 @@ import com.yarosh.checks.repository.entity.DiscountCardEntity;
 import com.yarosh.checks.repository.pool.DatabaseConnectionPool;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class DiscountCardRepository implements CrudRepository<DiscountCardEntity, Long> {
+public class JdbcDiscountCardRepository implements CrudRepository<DiscountCardEntity, Long> {
 
-    private static final String SQL_SELECT_ALL = "";
+    private static final String SQL_SELECT_ALL = "SELECT id, discount FROM discount_cards";
+
+    private static final String DISCOUNT_CARDS_ID_FIELD = "id";
+    private static final String DISCOUNT_CARDS_DISCOUNT_FIELD = "discount";
 
     private final DatabaseConnectionPool connectionPool;
 
-    public DiscountCardRepository(DatabaseConnectionPool connectionPool) {
+    public JdbcDiscountCardRepository(DatabaseConnectionPool connectionPool) {
         this.connectionPool = connectionPool;
     }
 
@@ -34,16 +37,22 @@ public class DiscountCardRepository implements CrudRepository<DiscountCardEntity
 
     @Override
     public List<DiscountCardEntity> findAll() {
-
         try (Connection connection = connectionPool.getConnection();
              Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery(SQL_SELECT_ALL);
+             ResultSet resultSet = statement.executeQuery(SQL_SELECT_ALL)
         ) {
+            List<DiscountCardEntity> cards = new ArrayList<>();
+            while (resultSet.next()) {
+                DiscountCardEntity card = new DiscountCardEntity(
+                        resultSet.getLong(DISCOUNT_CARDS_ID_FIELD),
+                        resultSet.getDouble(DISCOUNT_CARDS_DISCOUNT_FIELD)
+                );
+                cards.add(card);
+            }
+            return cards;
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new JdbcRepositoryException("SQL select all query failed, e: {0}", e);
         }
-
-        return null;
     }
 
     @Override
