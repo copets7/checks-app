4d23d23031715f9907af61890a5066ccc6a14dc3
Andrey Yarosh <andreyarosh7@gmail.com>
2023-03-25 16:01:24 +0300

    ref: changed convertToDomain and convertToEntity in ProductConverter.java

--- src/main/java/com/yarosh/checks/service/util/ProductConverter.java
+++ src/main/java/com/yarosh/checks/service/util/ProductConverter.java
@@ -1,6 +1,7 @@
 package com.yarosh.checks.service.util;
 
 import com.yarosh.checks.domain.Product;
+import com.yarosh.checks.domain.id.ProductId;
 import com.yarosh.checks.repository.entity.ProductEntity;
 
 import java.util.Optional;
@@ -10,11 +11,22 @@ public class ProductConverter implements BidirectionalConverter<Product, Product
 
     @Override
     public Product convertToDomain(ProductEntity entity) {
-        return new Product(entity.getId(), entity.getDescription(), Optional.empty(), entity.getPrice(), entity.getDiscount());
+        return new Product(
+                Optional.of(new ProductId(entity.getId())),
+                entity.getDescription(),
+                Optional.empty(),
+                entity.getPrice(),
+                entity.getDiscount()
+        );
     }
 
     @Override
     public ProductEntity convertToEntity(Product product) {
-        return new ProductEntity(product.getId(), product.getDescription(), product.getPrice(), product.getDiscount());
+        return new ProductEntity(
+                product.getId().map(ProductId::getId).orElse(null),
+                product.getDescription(),
+                product.getPrice(),
+                product.getDiscount()
+        );
     }
 }
