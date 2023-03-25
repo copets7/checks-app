4103c28c9b3db82ba4a414f4dfc442b168c4ac4c
Andrey Yarosh <andreyarosh7@gmail.com>
2023-03-25 15:52:10 +0300

    fix: changed convertToDomain and convertToEntity methods in DiscountCardConverter.java

--- src/main/java/com/yarosh/checks/service/util/DiscountCardConverter.java
+++ src/main/java/com/yarosh/checks/service/util/DiscountCardConverter.java
@@ -1,17 +1,20 @@
 package com.yarosh.checks.service.util;
 
 import com.yarosh.checks.domain.DiscountCard;
+import com.yarosh.checks.domain.id.DiscountCardId;
 import com.yarosh.checks.repository.entity.DiscountCardEntity;
+
+import java.util.Optional;
 
 public class DiscountCardConverter implements BidirectionalConverter<DiscountCard, DiscountCardEntity> {
 
     @Override
     public DiscountCard convertToDomain(DiscountCardEntity entity) {
-        return new DiscountCard(entity.getId(), entity.getDiscount());
+        return new DiscountCard(Optional.of(new DiscountCardId(entity.getId())) , entity.getDiscount());
     }
 
     @Override
     public DiscountCardEntity convertToEntity(DiscountCard card) {
-        return new DiscountCardEntity(card.getId(), card.getDiscount());
+        return new DiscountCardEntity(card.getId().map(DiscountCardId::getId).orElse(null) , card.getDiscount());
     }
 }
