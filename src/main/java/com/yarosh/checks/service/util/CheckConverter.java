048e21525a8e53285961087495861c7aceb881cf
Andrey Yarosh <andreyarosh7@gmail.com>
2023-03-25 15:51:18 +0300

    fix: changed convertToDomain and convertToEntity methods in CheckConverter.java

--- src/main/java/com/yarosh/checks/service/util/CheckConverter.java
+++ src/main/java/com/yarosh/checks/service/util/CheckConverter.java
@@ -3,6 +3,7 @@ package com.yarosh.checks.service.util;
 import com.yarosh.checks.domain.Check;
 import com.yarosh.checks.domain.DiscountCard;
 import com.yarosh.checks.domain.Product;
+import com.yarosh.checks.domain.id.CheckId;
 import com.yarosh.checks.repository.entity.CheckEntity;
 import com.yarosh.checks.repository.entity.DiscountCardEntity;
 import com.yarosh.checks.repository.entity.ProductEntity;
@@ -22,24 +23,28 @@ public class CheckConverter implements BidirectionalConverter<Check, CheckEntity
 
     @Override
     public Check convertToDomain(CheckEntity entity) {
-        return new Check(entity.getId(),
+        return new Check(
+                Optional.of(new CheckId(entity.getId())),
                 entity.getMarketName(),
                 entity.getCashierName(),
                 entity.getDate(),
                 entity.getTime(),
                 entity.getProducts().stream().map(productConverter::convertToDomain).toList(),
-                Optional.ofNullable(discountCardConverter.convertToDomain(entity.getDiscountCard())));
+                Optional.ofNullable(discountCardConverter.convertToDomain(entity.getDiscountCard()))
+        );
     }
 
     @Override
     public CheckEntity convertToEntity(Check check) {
-        return new CheckEntity(check.getId(),
+        return new CheckEntity(
+                check.getId().map(CheckId::getId).orElse(null),
                 check.getMarketName(),
                 check.getCashierName(),
                 check.getDate(),
                 check.getTime(),
                 check.getProducts().stream().map(productConverter::convertToEntity).toList(),
                 check.getDiscountCard().map(discountCardConverter::convertToEntity).get(),
-                check.getTotalPrice());
+                check.getTotalPrice()
+        );
     }
 }
