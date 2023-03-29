ed9448a5e084bf54c0b3f88cd2f11a8d9e6f1f0b
Andrey Yarosh <andreyarosh7@gmail.com>
2023-03-29 20:57:05 +0300

    ref: SqlExecutorException.java.java moved to package executor

--- src/main/java/com/yarosh/checks/repository/jdbc/SqlExecutorException.java
+++
@@ -1,8 +0,0 @@
-package com.yarosh.checks.repository.jdbc;
-
-public class SqlExecutorException extends JdbcRepositoryException{
-
-    public SqlExecutorException(String templateMessage, Object... params) {
-        super(templateMessage, params);
-    }
-}
