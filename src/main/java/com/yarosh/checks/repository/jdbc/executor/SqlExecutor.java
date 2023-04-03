5895e04e6489915802df567baac9191d1caa963f
Andrey Yarosh <andreyarosh7@gmail.com>
2023-04-03 19:48:49 +0300

    feature: added fields NO_ROWS_AFFECTED, converterToEntity and added methods select, delete,executeWithoutResponse, performUpdateConsumer. ref: changed resultSetExecutor to customConverterToEntity

--- src/main/java/com/yarosh/checks/repository/jdbc/executor/SqlExecutor.java
+++ src/main/java/com/yarosh/checks/repository/jdbc/executor/SqlExecutor.java
@@ -16,60 +16,92 @@ import java.util.function.Function;
 
 public class SqlExecutor<E extends Entity, ID> {
 
-    private final DataSource dataSource;
+    private static final int NO_ROWS_AFFECTED = 0;
 
-    public SqlExecutor(DataSource dataSource) {
+    private final DataSource dataSource;
+    private final Function<ResultSet, E> converterToEntity;
+
+    public SqlExecutor(DataSource dataSource, Function<ResultSet, E> converterToEntity) {
         this.dataSource = dataSource;
+        this.converterToEntity = converterToEntity;
     }
 
-    public List<E> selectAll(String sql, Function<ResultSet, E> resultSetExecutor) {
+    @SuppressWarnings("UNCHECKED_CAST")
+    public E update(String sql, E entity, Function<E, List<Object>> customConverterToParams) {
+        List<Object> params = customConverterToParams.apply(entity);
+        return execute(sql, params, preparedStatement -> {
+            performUpdateConsumer((ID) params.get(params.size() - 1)).accept(preparedStatement);
+            return entity;
+        }).orElseThrow();
+    }
+
+    public List<E> selectAll(String sql) {
+        return selectAll(sql, converterToEntity);
+    }
+
+    public List<E> selectAll(String sql, Function<ResultSet, E> customConverterToEntity) {
         List<E> entities = new ArrayList<>();
 
         executeWithoutResponse(sql, preparedStatement -> {
-            performSelectAllConsumer(entities, resultSetExecutor).accept(preparedStatement);
+            performSelectAllConsumer(entities, customConverterToEntity).accept(preparedStatement);
             return null;
         });
 
         return entities;
     }
 
-    public Optional<E> select(String sql, ID id, Function<ResultSet, E> resultSetExecutor) {
-        return execute(sql, List.of(id), performSelectFunction(resultSetExecutor));
+    public Optional<E> select(String sql, ID id) {
+        return select(sql, id, converterToEntity);
     }
 
-    public void executeWithoutResponse(String sql, Function<PreparedStatement, E> resultSetExecutor) {
-        execute(sql, List.of(), resultSetExecutor, Statement.NO_GENERATED_KEYS);
+    public Optional<E> select(String sql, ID id, Function<ResultSet, E> customConverterToEntity) {
+        return execute(sql, List.of(id), performSelectFunction(customConverterToEntity));
     }
 
-    public Optional<E> execute(String sql, Function<PreparedStatement, E> resultSetExecutor, int statementKey) {
-        return execute(sql, List.of(), resultSetExecutor, statementKey);
+    public void delete(String sql, ID id) {
+        executeWithoutResponse(sql, List.of(id), preparedStatement -> {
+            performUpdateConsumer(id).accept(preparedStatement);
+            return null;
+        });
     }
 
-    public Optional<E> execute(String sql, Function<PreparedStatement, E> resultSetExecutor) {
-        return execute(sql, List.of(), resultSetExecutor, Statement.NO_GENERATED_KEYS);
+    public void executeWithoutResponse(String sql, Function<PreparedStatement, E> customConverterToEntity) {
+        executeWithoutResponse(sql, List.of(), customConverterToEntity);
     }
 
-    public Optional<E> execute(String sql, List<Object> params, Function<PreparedStatement, E> resultSetExecutor) {
-        return execute(sql, params, resultSetExecutor, Statement.NO_GENERATED_KEYS);
+    public void executeWithoutResponse(String sql, List<Object> params, Function<PreparedStatement, E> customConverterToEntity) {
+        execute(sql, params, customConverterToEntity);
     }
 
-    public Optional<E> execute(String sql, List<Object> params, Function<PreparedStatement, E> resultSetExecutor, int statementKey) {
+    public Optional<E> execute(String sql, Function<PreparedStatement, E> customConverterToEntity, int statementKey) {
+        return execute(sql, List.of(), customConverterToEntity, statementKey);
+    }
+
+    public Optional<E> execute(String sql, Function<PreparedStatement, E> customConverterToEntity) {
+        return execute(sql, List.of(), customConverterToEntity, Statement.NO_GENERATED_KEYS);
+    }
+
+    public Optional<E> execute(String sql, List<Object> params, Function<PreparedStatement, E> customConverterToEntity) {
+        return execute(sql, params, customConverterToEntity, Statement.NO_GENERATED_KEYS);
+    }
+
+    public Optional<E> execute(String sql, List<Object> params, Function<PreparedStatement, E> customConverterToEntity, int statementKey) {
         try (Connection connection = dataSource.getConnection();
-             PreparedStatement prepareStatement = connection.prepareStatement(sql, statementKey);
+             PreparedStatement prepareStatement = connection.prepareStatement(sql, statementKey)
         ) {
             setParams(prepareStatement, params);
-            return Optional.ofNullable(resultSetExecutor.apply(prepareStatement));
+            return Optional.ofNullable(customConverterToEntity.apply(prepareStatement));
         } catch (SQLException e) {
             throw new SqlExecutorException("SQL executing failed, e: {0} ", e);
         }
     }
 
-    private Consumer<PreparedStatement> performSelectAllConsumer(List<E> result, Function<ResultSet, E> resultSetExecutor) {
+    private Consumer<PreparedStatement> performSelectAllConsumer(List<E> result, Function<ResultSet, E> customConverterToEntity) {
         return preparedStatement -> {
             try (ResultSet resultSet = preparedStatement.executeQuery()) {
 
                 while (resultSet.next()) {
-                    result.add(resultSetExecutor.apply(resultSet));
+                    result.add(customConverterToEntity.apply(resultSet));
                 }
 
             } catch (SQLException e) {
@@ -89,12 +121,24 @@ public class SqlExecutor<E extends Entity, ID> {
         }
     }
 
-    private Function<PreparedStatement, E> performSelectFunction(Function<ResultSet, E> resultSetExecutor) {
+    private Function<PreparedStatement, E> performSelectFunction(Function<ResultSet, E> customConverterToEntity) {
         return preparedStatement -> {
             try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                return (resultSet.next()) ? resultSetExecutor.apply(resultSet) : null;
+                return (resultSet.next()) ? customConverterToEntity.apply(resultSet) : null;
             } catch (SQLException e) {
                 throw new SqlExecutorException("Performing result set for SQL select failed, e: {0}", e);
+            }
+        };
+    }
+
+    private Consumer<PreparedStatement> performUpdateConsumer(ID id) {
+        return preparedStatement -> {
+            try {
+                if (preparedStatement.executeUpdate() == NO_ROWS_AFFECTED) {
+                    throw new SqlExecutorException("There is no record with ID {0}", id);
+                }
+            } catch (SQLException e) {
+                throw new SqlExecutorException("Executing SQL update failed, e: {0}", e);
             }
         };
     }
