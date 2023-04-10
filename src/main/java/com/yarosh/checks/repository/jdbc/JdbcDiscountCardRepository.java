09e4fae9957236df3422b91743c0f352ed2525ce
Andrey Yarosh <andreyarosh7@gmail.com>
2023-04-10 17:43:02 +0300

    ref: logs have been refactored feature: logs have been added

--- src/main/java/com/yarosh/checks/repository/jdbc/JdbcDiscountCardRepository.java
+++ src/main/java/com/yarosh/checks/repository/jdbc/JdbcDiscountCardRepository.java
@@ -27,46 +27,55 @@ public class JdbcDiscountCardRepository implements CrudRepository<DiscountCardEn
     private static final String DISCOUNT_CARDS_DISCOUNT_FIELD = "discount";
     private static final int GENERATED_KEY_COLUMN_NUMBER = 1;
 
-    private final DataSource dataSource;
     private final SqlExecutor<DiscountCardEntity, Long> sqlExecutor;
 
     public JdbcDiscountCardRepository(DataSource dataSource) {
-        this.dataSource = dataSource;
         this.sqlExecutor = new SqlExecutor<>(dataSource, this::convertToEntity, this::convertToParams);
     }
 
     @Override
     public DiscountCardEntity insert(DiscountCardEntity card) {
-        LOGGER.info("JdbcDiscountCardRepository starts inserting discount card");
-        return sqlExecutor.insert(SQL_INSERT, card, this::convertToEntity);
+        LOGGER.debug("JDBC SQL card inserting starts, card: {}", card);
+        DiscountCardEntity inserted = sqlExecutor.insert(SQL_INSERT, card, this::convertToEntity);
+        LOGGER.debug("JDBC SQL card inserting processed, ID: {}", inserted.getId());
+        LOGGER.trace("Inserted discount card: {}", inserted);
+
+        return inserted;
     }
 
     @Override
     public Optional<DiscountCardEntity> select(Long id) {
-        LOGGER.info("JdbcDiscountCardRepository starts searching discount card by id");
-        return sqlExecutor.select(SQL_SELECT, id);
+        LOGGER.debug("JDBC SQL discount card searching by id starts, id: {}", id);
+        Optional<DiscountCardEntity> selected = sqlExecutor.select(SQL_SELECT, id);
+        LOGGER.debug("JDBC SQL discount card searching by id processed, ID: {}", selected.get().getId());
+        LOGGER.trace("Selected discount card: {}", selected);
+
+        return selected;
     }
 
     @Override
     public List<DiscountCardEntity> selectAll() {
-        LOGGER.info("JdbcDiscountCardRepository starts selecting all discount cards");
+        LOGGER.debug("JDBC SQL selecting all discount cards starts");
         return sqlExecutor.selectAll(SQL_SELECT_ALL);
     }
 
     @Override
     public DiscountCardEntity update(DiscountCardEntity card) {
-        LOGGER.info("JdbcDiscountCardRepository starts updating discount card");
-        return sqlExecutor.update(SQL_UPDATE, card);
+        LOGGER.debug("JDBC SQL updating discount card starts, discount card: {}", card);
+        DiscountCardEntity updated = sqlExecutor.update(SQL_UPDATE, card);
+        LOGGER.debug("JDBC SQL updating discount card processed, product: {}", updated);
+        LOGGER.trace("Updated discount card: {}", updated);
+
+        return updated;
     }
 
     @Override
     public void delete(Long id) {
-        LOGGER.info("JdbcDiscountCardRepository starts deleting discount card");
+        LOGGER.debug("JDBC SQL deleting discount card starts, id: {}", id);
         sqlExecutor.delete(SQL_DELETE, id);
     }
 
     private DiscountCardEntity convertToEntity(ResultSet resultSet) {
-        LOGGER.debug("JdbcDiscountCardRepository starts convert Result set to discount card, result set: {}", resultSet);
         try {
             return new DiscountCardEntity(
                     resultSet.getLong(DISCOUNT_CARDS_ID_FIELD),
@@ -74,25 +83,23 @@ public class JdbcDiscountCardRepository implements CrudRepository<DiscountCardEn
             );
         } catch (SQLException e) {
             LOGGER.error("Converting result set to discount card after insert failed, message: {}", e.getMessage());
-            LOGGER.debug("Converting result set to discount card after insert failed, e: {0}", e);
+            LOGGER.debug("Converting result set to discount card after insert failed", e);
             throw new JdbcRepositoryException("Converting result set to discount card failed, e: {0}", e);
         }
     }
 
     private DiscountCardEntity convertToEntity(ResultSet resultSet, DiscountCardEntity card) {
-        LOGGER.debug("JdbcDiscountCardRepository starts convert Result set to discount card, result set: {}", resultSet);
-
         try {
             return new DiscountCardEntity(resultSet.getLong(GENERATED_KEY_COLUMN_NUMBER), card.getDiscount());
         } catch (SQLException e) {
             LOGGER.error("Converting result set to discount card after insert failed, message: {}", e.getMessage());
-            LOGGER.debug("Converting result set to discount card after insert failed, e: {0}", e);
+            LOGGER.debug("Converting result set to discount card after insert failed", e);
             throw new JdbcRepositoryException("Converting result set to discount card after insert failed, e: {0}", e);
         }
     }
 
     private List<Object> convertToParams(DiscountCardEntity card) {
-        LOGGER.debug("JdbcDiscountCardRepository starts convert card to params, card: {}", card);
+        LOGGER.trace("Converting card to params starts, card: {}", card);
         List<Object> params = new ArrayList<>();
         params.add(card.getDiscount());
 
@@ -100,7 +107,7 @@ public class JdbcDiscountCardRepository implements CrudRepository<DiscountCardEn
             params.add(card.getId());
         }
 
-        LOGGER.debug("JdbcDiscountCardRepository starts returning params, {}", params);
+        LOGGER.trace("Converted card to params returning, {}", params);
         return params;
     }
 }
