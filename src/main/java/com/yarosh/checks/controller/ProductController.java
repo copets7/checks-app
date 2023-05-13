ac51ab209c82e5c4a5e90dd590c9755408a0445c
Andrey Yarosh <andreyarosh7@gmail.com>
2023-05-13 21:39:37 +0300

    ref: renamed Converter to ApiDtoConverter, productConverter to productApiDtoConverter

--- src/main/java/com/yarosh/checks/controller/ProductController.java
+++ src/main/java/com/yarosh/checks/controller/ProductController.java
@@ -1,7 +1,7 @@
 package com.yarosh.checks.controller;
 
 import com.yarosh.checks.controller.dto.ProductDto;
-import com.yarosh.checks.controller.util.Converter;
+import com.yarosh.checks.controller.util.ApiDtoConverter;
 import com.yarosh.checks.controller.view.ProductView;
 import com.yarosh.checks.domain.Product;
 import com.yarosh.checks.domain.id.ProductId;
@@ -11,45 +11,41 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/product")
 public class ProductController {
 
     private final CrudService<Product, ProductId> productService;
-    private final Converter<ProductDto, ProductView, Product> productConverter;
+    private final ApiDtoConverter<ProductDto, ProductView, Product> productApiDtoConverter;
 
-    public ProductController(CrudService<Product, ProductId> productService, Converter<ProductDto, ProductView, Product> productConverter) {
+    public ProductController(CrudService<Product, ProductId> productService,
+                             ApiDtoConverter<ProductDto, ProductView, Product> productApiDtoConverter) {
         this.productService = productService;
-        this.productConverter = productConverter;
+        this.productApiDtoConverter = productApiDtoConverter;
     }
 
     @RequestMapping(path = "/add", method = RequestMethod.POST)
     public ResponseEntity<ProductView> add(final @RequestBody ProductDto productDto) {
-        final Product product = productConverter.dtoConvertToDomain(productDto);
-        final ProductView productView = productConverter.dtoConvertToView(productDto);
+        final Product product = productApiDtoConverter.convertDtoToDomain(productDto);
+        final Product created = productService.add(product);
 
-        productService.add(product);
-
-        return new ResponseEntity<>(productView, HttpStatus.CREATED);
+        return new ResponseEntity<>(productApiDtoConverter.convertDomainToView(created), HttpStatus.CREATED);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<ProductView> getById(final @PathVariable("id") Long id) {
-        Optional<ProductView> productView = productService.get(new ProductId(id))
-                .map(productConverter::domainConvertToView);
-
-        return productView.isPresent()
-                ? new ResponseEntity<>(productView.get(), HttpStatus.OK)
-                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return productService.get(new ProductId(id))
+                .map(productApiDtoConverter::convertDomainToView)
+                .map(view -> new ResponseEntity<>(view, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @RequestMapping(path = "/all", method = RequestMethod.GET)
     public ResponseEntity<List<ProductView>> getAll() {
         List<ProductView> products = productService.getAll()
                 .stream()
-                .map(productConverter::domainConvertToView)
+                .map(productApiDtoConverter::convertDomainToView)
                 .toList();
 
         return new ResponseEntity<>(products, HttpStatus.OK);
@@ -57,17 +53,14 @@ public class ProductController {
 
     @RequestMapping(path = "/update", method = RequestMethod.PUT)
     public ResponseEntity<ProductView> update(final @RequestBody ProductDto productDto) {
-        final Product product = productConverter.dtoConvertToDomain(productDto);
-        final ProductView productView = productConverter.dtoConvertToView(productDto);
+        final Product product = productApiDtoConverter.convertDtoToDomain(productDto);
+        final Product updated = productService.update(product);
 
-        productService.update(product);
-
-        return new ResponseEntity<>(productView, HttpStatus.OK);
+        return new ResponseEntity<>(productApiDtoConverter.convertDomainToView(updated), HttpStatus.OK);
     }
 
-    @RequestMapping(path = "/delete/{id}", method = RequestMethod.DELETE)
+    @RequestMapping(path = "/{id}/delete", method = RequestMethod.DELETE)
     public ResponseEntity<ProductView> delete(final @PathVariable Long id) {
-
         productService.delete(new ProductId(id));
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
