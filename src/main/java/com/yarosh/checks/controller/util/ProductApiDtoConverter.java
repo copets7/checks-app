b10164b5519d1b461701978acbd732368add306b
Andrey Yarosh <andreyarosh7@gmail.com>
2023-05-13 21:32:24 +0300

    ref: renamed ProductConverter.java to ProductApiDtoConverter.java and refactoring methods

---
+++ src/main/java/com/yarosh/checks/controller/util/ProductApiDtoConverter.java
@@ -0,0 +1,31 @@
+package com.yarosh.checks.controller.util;
+
+import com.yarosh.checks.controller.dto.ProductDto;
+import com.yarosh.checks.controller.view.ProductView;
+import com.yarosh.checks.domain.Product;
+import com.yarosh.checks.domain.id.ProductId;
+
+import java.util.Optional;
+
+public class ProductApiDtoConverter implements ApiDtoConverter<ProductDto, ProductView, Product> {
+
+    @Override
+    public ProductView convertDomainToView(Product domain) {
+        return new ProductView(
+                domain.description(),
+                domain.price(),
+                domain.discount()
+        );
+    }
+
+    @Override
+    public Product convertDtoToDomain(ProductDto dto) {
+        return new Product(
+                Optional.of(new ProductId(dto.id())),
+                dto.description(),
+                Optional.empty(),
+                dto.price(),
+                dto.discount()
+        );
+    }
+}
