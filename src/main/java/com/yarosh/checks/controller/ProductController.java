9fbe93acbd0dc36b016ea02f46083bd8df7e5938
Andrey Yarosh <andreyarosh7@gmail.com>
2023-05-14 16:38:10 +0300

    feature: added field LOGGER and logged methods ref: changed @RequestMapping(path = "/product") to @RequestMapping(path = "api/v1.0/product")

--- src/main/java/com/yarosh/checks/controller/ProductController.java
+++ src/main/java/com/yarosh/checks/controller/ProductController.java
@@ -6,6 +6,8 @@ import com.yarosh.checks.controller.view.ProductView;
 import com.yarosh.checks.domain.Product;
 import com.yarosh.checks.domain.id.ProductId;
 import com.yarosh.checks.service.CrudService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,8 +15,10 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/product")
+@RequestMapping(path = "api/v1.0/product")
 public class ProductController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
 
     private final CrudService<Product, ProductId> productService;
     private final ApiDtoConverter<ProductDto, ProductView, Product> productApiDtoConverter;
@@ -27,14 +31,22 @@ public class ProductController {
 
     @RequestMapping(path = "/add", method = RequestMethod.POST)
     public ResponseEntity<ProductView> add(final @RequestBody ProductDto productDto) {
+        LOGGER.info("Calling add started");
+        LOGGER.info("Product parameter: {}", productDto);
+
         final Product product = productApiDtoConverter.convertDtoToDomain(productDto);
         final Product created = productService.add(product);
+
+        LOGGER.info("Calling add successfully ended for product, id: {}", created.id());
+        LOGGER.debug("Saved product view detailed printing: {}", created);
 
         return new ResponseEntity<>(productApiDtoConverter.convertDomainToView(created), HttpStatus.CREATED);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<ProductView> getById(final @PathVariable("id") Long id) {
+        LOGGER.info("Calling getById started for id: {}", id);
+
         return productService.get(new ProductId(id))
                 .map(productApiDtoConverter::convertDomainToView)
                 .map(view -> new ResponseEntity<>(view, HttpStatus.OK))
@@ -43,24 +55,35 @@ public class ProductController {
 
     @RequestMapping(path = "/all", method = RequestMethod.GET)
     public ResponseEntity<List<ProductView>> getAll() {
+        LOGGER.info("Calling getAll started");
+
         List<ProductView> products = productService.getAll()
                 .stream()
                 .map(productApiDtoConverter::convertDomainToView)
                 .toList();
+
+        LOGGER.debug("Product views detailed printing: {}", products);
 
         return new ResponseEntity<>(products, HttpStatus.OK);
     }
 
     @RequestMapping(path = "/update", method = RequestMethod.PUT)
     public ResponseEntity<ProductView> update(final @RequestBody ProductDto productDto) {
+        LOGGER.info("Calling update started for product with id: {}", productDto.id());
+        LOGGER.info("Product parameter: {}", productDto);
+
         final Product product = productApiDtoConverter.convertDtoToDomain(productDto);
         final Product updated = productService.update(product);
+
+        LOGGER.info("Calling updated successfully ended for product, id: {}", updated.id());
+        LOGGER.debug("Updated product view detailed printing: {}", updated);
 
         return new ResponseEntity<>(productApiDtoConverter.convertDomainToView(updated), HttpStatus.OK);
     }
 
     @RequestMapping(path = "/{id}/delete", method = RequestMethod.DELETE)
     public ResponseEntity<ProductView> delete(final @PathVariable Long id) {
+        LOGGER.info("Calling delete started for product, id: {}", id);
         productService.delete(new ProductId(id));
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
