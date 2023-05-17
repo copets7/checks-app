9d26b24f1a78bfc87fc4bbfa58d4fdd771a8c0dd
Andrey Yarosh <andreyarosh7@gmail.com>
2023-05-17 22:42:48 +0300

    ref: changed DiscountCardId.java to record

--- src/main/java/com/yarosh/checks/domain/id/DiscountCardId.java
+++ src/main/java/com/yarosh/checks/domain/id/DiscountCardId.java
@@ -2,17 +2,11 @@ package com.yarosh.checks.domain.id;
 
 import com.yarosh.checks.domain.exception.TypedIdException;
 
-public class DiscountCardId implements DomainId {
-
-    private final Long id;
+public record DiscountCardId(Long id) implements DomainId {
 
     public DiscountCardId(Long id) {
         this.id = id;
         validate();
-    }
-
-    public Long getId() {
-        return id;
     }
 
     @Override
