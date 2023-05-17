2db1998748108854fe3fd2f443086cf0b72f7d07
Andrey Yarosh <andreyarosh7@gmail.com>
2023-05-17 22:45:02 +0300

    feature: added AppExceptionHandler.java

---
+++ src/main/java/com/yarosh/checks/controller/AppExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.yarosh.checks.controller;
+
+import com.yarosh.checks.domain.exception.InvalidProductException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class AppExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AppExceptionHandler.class);
+
+    @ExceptionHandler(InvalidProductException.class)
+    protected ResponseEntity<String> handleInvalidProductException(final InvalidProductException e) {
+        LOGGER.debug("InvalidProductException was handled, message {}", e.getMessage());
+        LOGGER.trace("InvalidProductException was handled", e);
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.PRECONDITION_FAILED);
+    }
+}
