cfcb5bc363eace14fdd36bc11573bdbb8f204e01
Andrey Yarosh <andreyarosh7@gmail.com>
2023-05-17 22:38:34 +0300

    ref: changed class ProductId.java to record

--- src/main/java/com/yarosh/checks/domain/id/ProductId.java
+++ src/main/java/com/yarosh/checks/domain/id/ProductId.java
@@ -2,17 +2,11 @@ package com.yarosh.checks.domain.id;
 
 import com.yarosh.checks.domain.exception.TypedIdException;
 
-public class ProductId implements DomainId {
-
-    private final Long id;
+public record ProductId(Long id) implements DomainId {
 
     public ProductId(Long id) {
         this.id = id;
         validate();
-    }
-
-    public Long getId() {
-        return id;
     }
 
     @Override
