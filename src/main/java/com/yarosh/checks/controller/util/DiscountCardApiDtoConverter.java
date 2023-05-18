8da878806ea89989b63c0955275f827ea232963a
Andrey Yarosh <andreyarosh7@gmail.com>
2023-05-18 18:28:19 +0300

    feature: added DiscountCardApiDtoConverter.java

---
+++ src/main/java/com/yarosh/checks/controller/util/DiscountCardApiDtoConverter.java
@@ -0,0 +1,26 @@
+package com.yarosh.checks.controller.util;
+
+import com.yarosh.checks.controller.dto.DiscountCardDto;
+import com.yarosh.checks.controller.view.DiscountCardView;
+import com.yarosh.checks.domain.DiscountCard;
+import com.yarosh.checks.domain.id.DiscountCardId;
+
+import java.util.Optional;
+
+public class DiscountCardApiDtoConverter implements ApiDtoConverter<DiscountCardDto, DiscountCardView, DiscountCard> {
+
+    @Override
+    public DiscountCardView convertDomainToView(DiscountCard domain) {
+        return new DiscountCardView(
+                domain.id().orElseThrow().id(),
+                domain.discount()
+        );
+    }
+
+    @Override
+    public DiscountCard convertDtoToDomain(DiscountCardDto dto) {
+        return new DiscountCard(
+                Optional.ofNullable(dto.id()).map(DiscountCardId::new),
+                dto.discount());
+    }
+}
