7adf1e224620c84d1ea7b9adb475f41cb5c23a82
Andrey Yarosh <andreyarosh7@gmail.com>
2023-05-23 19:23:31 +0300

    feature: added handleRuntimeException method

--- src/main/java/com/yarosh/checks/controller/handler/UnexpectedExceptionHandler.java
+++ src/main/java/com/yarosh/checks/controller/handler/UnexpectedExceptionHandler.java
@@ -2,7 +2,10 @@ package com.yarosh.checks.controller.handler;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
@@ -10,5 +13,10 @@ public class UnexpectedExceptionHandler extends ResponseEntityExceptionHandler {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UnexpectedExceptionHandler.class);
 
-
+    @ExceptionHandler(RuntimeException.class)
+    protected ResponseEntity<String> handleRuntimeException(final RuntimeException e) {
+        LOGGER.debug("RuntimeException was handled, message {}", e.getMessage());
+        LOGGER.trace("RuntimeException was handled", e);
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.SERVICE_UNAVAILABLE);
+    }
 }
