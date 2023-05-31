4684dad49457f45766d5b65f0a0e135c45ddf716
Andrey Yarosh <andreyarosh7@gmail.com>
2023-05-31 22:29:20 +0300

    feature: added WebSecurityConfig.java

---
+++ services/checks/src/main/java/com/yarosh/checks/controller/security/WebSecurityConfig.java
@@ -0,0 +1,34 @@
+package com.yarosh.checks.controller.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final BasicAuthenticationEntryPoint basicAuthenticationEntryPoint = new AppBasicAuthenticationEntryPoint();
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authManager) throws Exception {
+        authManager.inMemoryAuthentication()
+                .withUser("Cashier")
+                .password(new BCryptPasswordEncoder().encode("pass"))
+                .authorities("ROLE_USER");
+    }
+
+    @Override
+    public void configure(HttpSecurity security) throws Exception {
+        security.csrf()
+                .disable()
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .httpBasic().authenticationEntryPoint(basicAuthenticationEntryPoint);
+    }
+}
