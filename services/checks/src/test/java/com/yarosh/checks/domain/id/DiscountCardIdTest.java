2a997fb06ec5cf418ce8695fba92651c09620403
Andrey Yarosh <andreyarosh7@gmail.com>
2023-06-05 22:01:26 +0300

    feature: added DiscountCardIdTest.java and tests

---
+++ services/checks/src/test/java/com/yarosh/checks/domain/id/DiscountCardIdTest.java
@@ -0,0 +1,27 @@
+package com.yarosh.checks.domain.id;
+
+import com.yarosh.checks.domain.exception.TypedIdException;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DiscountCardIdTest {
+
+    @Test
+    public void createDiscountCardId() {
+        //given
+        new DiscountCardId(1L);
+        //expected nothing
+    }
+
+    @Test
+    public void createDiscountCardIdAndThrowTypedIdException() {
+        TypedIdException e = assertThrows(TypedIdException.class, () -> {
+            new DiscountCardId(null);
+        });
+
+        final String expectedMessage = "ID can not be null in";
+        final String actualMessage = e.getMessage();
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+}
