6796e48b99a340386b9ab61864fed2f1783a63b0
Andrey Yarosh <andreyarosh7@gmail.com>
2023-06-05 22:01:42 +0300

    feature: added CheckIdTest.java and tests

---
+++ services/checks/src/test/java/com/yarosh/checks/domain/id/CheckIdTest.java
@@ -0,0 +1,28 @@
+package com.yarosh.checks.domain.id;
+
+import com.yarosh.checks.domain.exception.TypedIdException;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CheckIdTest {
+
+    @Test
+    public void createCheckId() {
+        //given
+        new CheckId(1L);
+        //expected nothing
+    }
+
+    @Test
+    public void createCheckIdAndThrowTypedIdException() {
+        TypedIdException e = assertThrows(TypedIdException.class, () -> {
+            new CheckId(null);
+        });
+
+        final String expectedMessage = "ID can not be null in";
+        final String actualMessage = e.getMessage();
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+}
