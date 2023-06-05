3070306caa2808d9300399b6da122ff5e6d70068
Andrey Yarosh <andreyarosh7@gmail.com>
2023-06-05 10:33:52 +0300

    ref: cleared imports

--- services/checks/src/main/java/com/yarosh/checks/controller/security/WebSecurityConfig.java
+++ services/checks/src/main/java/com/yarosh/checks/controller/security/WebSecurityConfig.java
@@ -1,12 +1,10 @@
 package com.yarosh.checks.controller.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 
 @EnableWebSecurity
