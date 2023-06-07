93278b9b673434e4999209482bed69fbf0078a9e
Andrey Yarosh <andreyarosh7@gmail.com>
2023-06-07 23:42:28 +0300

    ref: removed convertToView and convertDtoToDomain methods

--- services/checks/src/main/java/com/yarosh/checks/service/util/ProductConverter.java
+++ services/checks/src/main/java/com/yarosh/checks/service/util/ProductConverter.java
@@ -1,8 +1,5 @@
 package com.yarosh.checks.service.util;
 
-import com.yarosh.checks.controller.dto.Dto;
-import com.yarosh.checks.controller.dto.ProductDto;
-import com.yarosh.checks.controller.view.ProductView;
 import com.yarosh.checks.domain.Product;
 import com.yarosh.checks.domain.id.ProductId;
 import com.yarosh.library.repository.api.entity.ProductEntity;
@@ -10,7 +7,7 @@ import com.yarosh.library.repository.api.entity.ProductEntity;
 import java.util.Optional;
 
 
-public class ProductConverter implements BidirectionalConverter<Product, ProductEntity, ProductView, ProductDto> {
+public class ProductConverter implements BidirectionalConverter<Product, ProductEntity> {
 
     @Override
     public Product convertToDomain(ProductEntity entity) {
@@ -29,26 +26,6 @@ public class ProductConverter implements BidirectionalConverter<Product, Product
                 product.description(),
                 product.price(),
                 product.discount()
-        );
-    }
-
-    @Override
-    public ProductView convertToView(Product product) {
-        return new ProductView(
-                product.id().map(ProductId::id).orElse(null),
-                product.description(),
-                product.price(),
-                product.discount()
-        );
-    }
-
-    @Override
-    public Product convertDtoToDomain(ProductDto dto) {
-        return new Product(
-                Optional.of(new ProductId(dto.id())),
-                dto.description(),
-                dto.price(),
-                dto.discount()
         );
     }
 }
