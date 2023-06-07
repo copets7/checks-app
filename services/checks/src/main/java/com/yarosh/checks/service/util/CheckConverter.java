27ca23ffde9a3943889d30412e80789915c1296d
Andrey Yarosh <andreyarosh7@gmail.com>
2023-06-07 22:12:55 +0300

    feature: added convertToView and convertToProductsView methods

--- services/checks/src/main/java/com/yarosh/checks/service/util/CheckConverter.java
+++ services/checks/src/main/java/com/yarosh/checks/service/util/CheckConverter.java
@@ -1,5 +1,8 @@
 package com.yarosh.checks.service.util;
 
+import com.yarosh.checks.controller.view.CheckView;
+import com.yarosh.checks.controller.view.DiscountCardView;
+import com.yarosh.checks.controller.view.ProductView;
 import com.yarosh.checks.domain.Check;
 import com.yarosh.checks.domain.DiscountCard;
 import com.yarosh.checks.domain.Product;
@@ -12,13 +15,13 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-public class CheckConverter implements BidirectionalConverter<Check, CheckEntity> {
+public class CheckConverter implements BidirectionalConverter<Check, CheckEntity, CheckView> {
 
-    private final BidirectionalConverter<Product, ProductEntity> productConverter;
-    private final BidirectionalConverter<DiscountCard, DiscountCardEntity> discountCardConverter;
+    private final BidirectionalConverter<Product, ProductEntity, ProductView> productConverter;
+    private final BidirectionalConverter<DiscountCard, DiscountCardEntity, DiscountCardView> discountCardConverter;
 
-    public CheckConverter(final BidirectionalConverter<Product, ProductEntity> productConverter,
-                          final BidirectionalConverter<DiscountCard, DiscountCardEntity> discountCardConverter) {
+    public CheckConverter(final BidirectionalConverter<Product, ProductEntity, ProductView> productConverter,
+                          final BidirectionalConverter<DiscountCard, DiscountCardEntity, DiscountCardView> discountCardConverter) {
         this.productConverter = productConverter;
         this.discountCardConverter = discountCardConverter;
     }
@@ -50,6 +53,19 @@ public class CheckConverter implements BidirectionalConverter<Check, CheckEntity
         );
     }
 
+    @Override
+    public CheckView convertToView(Check check) {
+        return new CheckView(
+                check.getId().map(CheckId::id).orElse(null),
+                check.getMarketName(),
+                check.getCashierName(),
+                check.getDate(),
+                check.getTime(),
+                convertToProductsView(check.getProducts()),
+                check.getDiscountCard().map(discountCardConverter::convertToView).orElseThrow()
+        );
+    }
+
     private Map<Product, Integer> convertToProducts(Map<ProductEntity, Integer> products) {
         return products.entrySet()
                 .stream()
@@ -60,5 +76,11 @@ public class CheckConverter implements BidirectionalConverter<Check, CheckEntity
         return products.entrySet()
                 .stream()
                 .collect(Collectors.toMap(entry -> productConverter.convertToEntity(entry.getKey()), Map.Entry::getValue));
+    }
+
+    private Map<ProductView, Integer> convertToProductsView(Map<Product, Integer> products) {
+        return products.entrySet()
+                .stream()
+                .collect(Collectors.toMap(entry -> productConverter.convertToView(entry.getKey()), Map.Entry::getValue));
     }
 }
