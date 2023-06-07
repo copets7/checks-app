1a2c0fab23dcfe1354fd1ae3b85934c08db045fc
Andrey Yarosh <andreyarosh7@gmail.com>
2023-06-07 21:42:36 +0300

    feature: added method V convertToView(D domain);

--- services/checks/src/main/java/com/yarosh/checks/service/util/BidirectionalConverter.java
+++ services/checks/src/main/java/com/yarosh/checks/service/util/BidirectionalConverter.java
@@ -1,11 +1,14 @@
 package com.yarosh.checks.service.util;
 
+import com.yarosh.checks.controller.view.View;
 import com.yarosh.checks.domain.Domain;
 import com.yarosh.library.repository.api.entity.Entity;
 
-public interface BidirectionalConverter<D extends Domain, E extends Entity> {
+public interface BidirectionalConverter<D extends Domain, E extends Entity,V extends View> {
 
     D convertToDomain(E entity);
 
     E convertToEntity(D domain);
+
+    V convertToView(D domain);
 }
