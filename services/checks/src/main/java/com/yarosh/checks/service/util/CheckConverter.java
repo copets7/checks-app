1548299bcb1e8a8f1fb41922088ef3cf1fae13c1
Andrey Yarosh <andreyarosh7@gmail.com>
2023-06-07 23:02:50 +0300

    feature: implemented convertDtoToDomain and added convertDtoToProducts method

--- services/checks/src/main/java/com/yarosh/checks/service/util/CheckConverter.java
+++ services/checks/src/main/java/com/yarosh/checks/service/util/CheckConverter.java
@@ -1,5 +1,8 @@
 package com.yarosh.checks.service.util;
 
+import com.yarosh.checks.controller.dto.CheckDto;
+import com.yarosh.checks.controller.dto.DiscountCardDto;
+import com.yarosh.checks.controller.dto.ProductDto;
 import com.yarosh.checks.controller.view.CheckView;
 import com.yarosh.checks.controller.view.DiscountCardView;
 import com.yarosh.checks.controller.view.ProductView;
@@ -15,13 +18,13 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-public class CheckConverter implements BidirectionalConverter<Check, CheckEntity, CheckView> {
+public class CheckConverter implements BidirectionalConverter<Check, CheckEntity, CheckView, CheckDto> {
 
-    private final BidirectionalConverter<Product, ProductEntity, ProductView> productConverter;
-    private final BidirectionalConverter<DiscountCard, DiscountCardEntity, DiscountCardView> discountCardConverter;
+    private final BidirectionalConverter<Product, ProductEntity, ProductView, ProductDto> productConverter;
+    private final BidirectionalConverter<DiscountCard, DiscountCardEntity, DiscountCardView, DiscountCardDto> discountCardConverter;
 
-    public CheckConverter(final BidirectionalConverter<Product, ProductEntity, ProductView> productConverter,
-                          final BidirectionalConverter<DiscountCard, DiscountCardEntity, DiscountCardView> discountCardConverter) {
+    public CheckConverter(final BidirectionalConverter<Product, ProductEntity, ProductView, ProductDto> productConverter,
+                          final BidirectionalConverter<DiscountCard, DiscountCardEntity, DiscountCardView, DiscountCardDto> discountCardConverter) {
         this.productConverter = productConverter;
         this.discountCardConverter = discountCardConverter;
     }
@@ -66,6 +69,19 @@ public class CheckConverter implements BidirectionalConverter<Check, CheckEntity
         );
     }
 
+    @Override
+    public Check convertDtoToDomain(CheckDto dto) {
+        return new Check(
+                Optional.of(new CheckId(dto.id())),
+                dto.marketName(),
+                dto.cashierName(),
+                dto.date(),
+                dto.time(),
+                convertDtoToProducts(dto.products()),
+                Optional.of(dto.discountCard().map(discountCardConverter::convertDtoToDomain).orElseThrow())
+        );
+    }
+
     private Map<Product, Integer> convertToProducts(Map<ProductEntity, Integer> products) {
         return products.entrySet()
                 .stream()
@@ -82,5 +98,11 @@ public class CheckConverter implements BidirectionalConverter<Check, CheckEntity
         return products.entrySet()
                 .stream()
                 .collect(Collectors.toMap(entry -> productConverter.convertToView(entry.getKey()), Map.Entry::getValue));
+    }
+
+    private Map<Product, Integer> convertDtoToProducts(Map<ProductDto, Integer> products) {
+        return products.entrySet()
+                .stream()
+                .collect(Collectors.toMap(entry -> productConverter.convertDtoToDomain(entry.getKey()), Map.Entry::getValue));
     }
 }
