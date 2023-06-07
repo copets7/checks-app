13ab8da463acc0886b795bf1b60e4c19b6c11984
Andrey Yarosh <andreyarosh7@gmail.com>
2023-06-07 23:36:53 +0300

    ref: removed convertToView and convertDtoToDomain methods

--- services/checks/src/main/java/com/yarosh/checks/service/util/BidirectionalConverter.java
+++ services/checks/src/main/java/com/yarosh/checks/service/util/BidirectionalConverter.java
@@ -1,17 +1,13 @@
 package com.yarosh.checks.service.util;
 
-import com.yarosh.checks.controller.dto.Dto;
-import com.yarosh.checks.controller.view.View;
 import com.yarosh.checks.domain.Domain;
 import com.yarosh.library.repository.api.entity.Entity;
 
-public interface BidirectionalConverter<D extends Domain, E extends Entity,V extends View, DTO extends Dto> {
+public interface BidirectionalConverter<D extends Domain, E extends Entity> {
 
     D convertToDomain(E entity);
 
     E convertToEntity(D domain);
 
-    V convertToView(D domain);
 
-    D convertDtoToDomain(DTO dto);
 }
