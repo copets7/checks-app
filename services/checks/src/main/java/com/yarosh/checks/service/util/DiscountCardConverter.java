75eb7d80a5ec7d3d7a4699e87545b4fe52abce27
Andrey Yarosh <andreyarosh7@gmail.com>
2023-06-07 22:15:24 +0300

    feature: added convertToView method

--- services/checks/src/main/java/com/yarosh/checks/service/util/DiscountCardConverter.java
+++ services/checks/src/main/java/com/yarosh/checks/service/util/DiscountCardConverter.java
@@ -1,20 +1,26 @@
 package com.yarosh.checks.service.util;
 
+import com.yarosh.checks.controller.view.DiscountCardView;
 import com.yarosh.checks.domain.DiscountCard;
 import com.yarosh.checks.domain.id.DiscountCardId;
 import com.yarosh.library.repository.api.entity.DiscountCardEntity;
 
 import java.util.Optional;
 
-public class DiscountCardConverter implements BidirectionalConverter<DiscountCard, DiscountCardEntity> {
+public class DiscountCardConverter implements BidirectionalConverter<DiscountCard, DiscountCardEntity, DiscountCardView> {
 
     @Override
     public DiscountCard convertToDomain(DiscountCardEntity entity) {
-        return new DiscountCard(Optional.of(new DiscountCardId(entity.getId())) , entity.getDiscount());
+        return new DiscountCard(Optional.of(new DiscountCardId(entity.getId())), entity.getDiscount());
     }
 
     @Override
     public DiscountCardEntity convertToEntity(DiscountCard card) {
-        return new DiscountCardEntity(card.id().map(DiscountCardId::id).orElse(null) , card.discount());
+        return new DiscountCardEntity(card.id().map(DiscountCardId::id).orElse(null), card.discount());
+    }
+
+    @Override
+    public DiscountCardView convertToView(DiscountCard card) {
+        return new DiscountCardView(card.id().map(DiscountCardId::id).orElse(null), card.discount());
     }
 }
