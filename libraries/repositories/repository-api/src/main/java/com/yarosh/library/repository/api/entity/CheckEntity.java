78625ffee214480bcf95cabb24881187e4271783
Andrey Yarosh <andreyarosh7@gmail.com>
2023-06-14 19:08:55 +0300

    ref: removed imports

--- libraries/repositories/repository-api/src/main/java/com/yarosh/library/repository/api/entity/CheckEntity.java
+++ libraries/repositories/repository-api/src/main/java/com/yarosh/library/repository/api/entity/CheckEntity.java
@@ -2,7 +2,6 @@ package com.yarosh.library.repository.api.entity;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.List;
 import java.util.Map;
 
 public class CheckEntity implements Entity {
