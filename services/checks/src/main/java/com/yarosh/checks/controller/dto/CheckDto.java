015fbc9f955242972aadee0cf3d03c861fe93233
Andrey Yarosh <andreyarosh7@gmail.com>
2023-06-14 19:07:55 +0300

    ref: removed and refactoring fields

--- services/checks/src/main/java/com/yarosh/checks/controller/dto/CheckDto.java
+++ services/checks/src/main/java/com/yarosh/checks/controller/dto/CheckDto.java
@@ -8,40 +8,32 @@ import java.time.LocalTime;
 import java.util.Map;
 
 public record CheckDto(Long id,
-                       String marketName,
-                       String cashierName,
                        LocalDate date,
                        LocalTime time,
-                       Map<ProductDto, Integer> products,
-                       DiscountCardDto discountCard) implements Dto {
+                       Map<Long, Integer> products,
+                       Long discountCardId) implements Dto {
 
     @JsonCreator
     public CheckDto(final @JsonProperty("id") Long id,
-                    final @JsonProperty("market_name") String marketName,
-                    final @JsonProperty("cashier_name") String cashierName,
                     final @JsonProperty("date") LocalDate date,
                     final @JsonProperty("time") LocalTime time,
-                    final @JsonProperty("products") Map<ProductDto, Integer> products,
-                    final @JsonProperty("discount_card_id") DiscountCardDto discountCard) {
+                    final @JsonProperty("products") Map<Long, Integer> products,
+                    final @JsonProperty("discount_card_id") Long discountCardId) {
         this.id = id;
-        this.marketName = marketName;
-        this.cashierName = cashierName;
         this.date = date;
         this.time = time;
         this.products = products;
-        this.discountCard = discountCard;
+        this.discountCardId = discountCardId;
     }
 
     @Override
     public String toString() {
         return "CheckDto{" +
                 "id=" + id +
-                ", marketName='" + marketName + '\'' +
-                ", cashierName='" + cashierName + '\'' +
                 ", date=" + date +
                 ", time=" + time +
                 ", products=" + products +
-                ", discountCard=" + discountCard +
+                ", discountCard=" + discountCardId +
                 '}';
     }
 }
