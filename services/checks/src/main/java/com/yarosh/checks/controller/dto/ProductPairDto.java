363079334ff37f9db63919eb6eda6eb091c618fa
Andrey Yarosh <andreyarosh7@gmail.com>
2023-06-17 14:49:00 +0300

    feature: added ProductPairDto.java

---
+++ services/checks/src/main/java/com/yarosh/checks/controller/dto/ProductPairDto.java
@@ -0,0 +1,28 @@
+package com.yarosh.checks.controller.dto;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record ProductPairDto(@JsonProperty("id") Long id, @JsonProperty("quantity_in_check") Integer quantityInCheck) {
+
+    @JsonCreator
+    public ProductPairDto { }
+
+    @Override
+    public Long id() {
+        return id;
+    }
+
+    @Override
+    public Integer quantityInCheck() {
+        return quantityInCheck;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductPairDto{" +
+                "id=" + id +
+                ", quantityInCheck=" + quantityInCheck +
+                '}';
+    }
+}
