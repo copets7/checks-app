5fa424a35bfff1cb6a58db81522e9823a264daa6
Andrey Yarosh <andreyarosh7@gmail.com>
2023-06-17 14:48:13 +0300

    feature: added ProductApiDtoConverterImpl.java

---
+++ services/checks/src/main/java/com/yarosh/checks/controller/util/ProductApiDtoConverterImpl.java
@@ -0,0 +1,37 @@
+package com.yarosh.checks.controller.util;
+
+import com.yarosh.checks.controller.dto.ProductDto;
+import com.yarosh.checks.controller.view.ProductView;
+import com.yarosh.checks.domain.Product;
+import com.yarosh.checks.domain.id.ProductId;
+
+import java.util.Optional;
+
+public class ProductApiDtoConverterImpl implements ProductApiDtoConverter {
+
+    @Override
+    public ProductView convertDomainToView(Product domain) {
+        return convertProductToView(domain, null);
+    }
+
+    @Override
+    public Product convertDtoToDomain(ProductDto dto) {
+        return new Product(
+                Optional.ofNullable(dto.id()).map(ProductId::new),
+                dto.description(),
+                dto.price(),
+                dto.discount()
+        );
+    }
+
+    @Override
+    public ProductView convertProductToView(Product product, Integer quantityInCheck) {
+        return new ProductView(
+                product.id().orElseThrow().id(),
+                product.description(),
+                product.price(),
+                product.discount(),
+                quantityInCheck
+        );
+    }
+}
