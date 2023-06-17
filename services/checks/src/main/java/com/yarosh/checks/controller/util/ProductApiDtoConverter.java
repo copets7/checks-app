c4e2dee45f10b82d6949c22adb86d9c4a947e435
Andrey Yarosh <andreyarosh7@gmail.com>
2023-06-17 14:48:43 +0300

    ref: changed class ProductApiDtoConverter to interface

--- services/checks/src/main/java/com/yarosh/checks/controller/util/ProductApiDtoConverter.java
+++ services/checks/src/main/java/com/yarosh/checks/controller/util/ProductApiDtoConverter.java
@@ -3,29 +3,8 @@ package com.yarosh.checks.controller.util;
 import com.yarosh.checks.controller.dto.ProductDto;
 import com.yarosh.checks.controller.view.ProductView;
 import com.yarosh.checks.domain.Product;
-import com.yarosh.checks.domain.id.ProductId;
 
-import java.util.Optional;
+public interface ProductApiDtoConverter extends ApiDtoConverter<ProductDto, ProductView, Product> {
 
-public class ProductApiDtoConverter implements ApiDtoConverter<ProductDto, ProductView, Product> {
-
-    @Override
-    public ProductView convertDomainToView(Product domain) {
-        return new ProductView(
-                domain.id().orElseThrow().id(),
-                domain.description(),
-                domain.price(),
-                domain.discount()
-        );
-    }
-
-    @Override
-    public Product convertDtoToDomain(ProductDto dto) {
-        return new Product(
-                Optional.ofNullable(dto.id()).map(ProductId::new),
-                dto.description(),
-                dto.price(),
-                dto.discount()
-        );
-    }
+    ProductView convertProductToView(Product product, Integer quantityInCheck);
 }
