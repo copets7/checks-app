bf561da6d1ce0c24bf2d912b95db489cc91ccc07
Andrey Yarosh <andreyarosh7@gmail.com>
2023-06-21 23:02:20 +0300

    ref: refactored imports, removed Beans checkSqlExecutor, discountCardSqlExecutor and productSqlExecutor

--- libraries/repositories/jdbc/src/main/java/com/yarosh/library/repository/jdbc/JdbcConfig.java
+++ libraries/repositories/jdbc/src/main/java/com/yarosh/library/repository/jdbc/JdbcConfig.java
@@ -5,17 +5,15 @@ import com.yarosh.library.repository.api.CrudRepository;
 import com.yarosh.library.repository.api.entity.CheckEntity;
 import com.yarosh.library.repository.api.entity.DiscountCardEntity;
 import com.yarosh.library.repository.api.entity.ProductEntity;
-import com.yarosh.library.repository.jdbc.executor.DefaultSqlExecutor;
-import com.yarosh.library.repository.jdbc.executor.SqlExecutor;
+import com.yarosh.library.repository.executor.SqlExecutor;
+import com.yarosh.library.repository.executor.SqlExecutorConfig;
 import com.yarosh.library.repository.pool.ConnectionPoolConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-import javax.sql.DataSource;
-
 @Configuration
-@Import(ConnectionPoolConfig.class)
+@Import({ConnectionPoolConfig.class, SqlExecutorConfig.class})
 public class JdbcConfig {
 
     @Bean
@@ -24,28 +22,13 @@ public class JdbcConfig {
     }
 
     @Bean
-    public SqlExecutor<CheckEntity, Long> checkSqlExecutor(DataSource dataSource) {
-        return new DefaultSqlExecutor<>(dataSource);
-    }
-
-    @Bean
     public CrudRepository<DiscountCardEntity, Long> discountCardRepository(SqlExecutor<DiscountCardEntity, Long> sqlExecutor) {
         return new JdbcDiscountCardRepository(sqlExecutor);
     }
 
     @Bean
-    public SqlExecutor<DiscountCardEntity, Long> discountCardSqlExecutor(DataSource dataSource) {
-        return new DefaultSqlExecutor<>(dataSource);
-    }
-
-    @Bean
     public CrudRepository<ProductEntity, Long> productRepository(SqlExecutor<ProductEntity, Long> sqlExecutor) {
         return new JdbcProductRepository(sqlExecutor);
-    }
-
-    @Bean
-    public SqlExecutor<ProductEntity, Long> productSqlExecutor(DataSource dataSource) {
-        return new DefaultSqlExecutor<>(dataSource);
     }
 
     @Bean
