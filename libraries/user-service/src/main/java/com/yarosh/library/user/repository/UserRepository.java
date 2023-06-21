e34d87a50d65ed03bdb38c36b461d0242ebe6097
Andrey Yarosh <andreyarosh7@gmail.com>
2023-06-21 23:04:15 +0300

    ref: changed class to interface

--- libraries/user-service/src/main/java/com/yarosh/library/user/repository/UserRepository.java
+++ libraries/user-service/src/main/java/com/yarosh/library/user/repository/UserRepository.java
@@ -1,31 +1,8 @@
 package com.yarosh.library.user.repository;
 
-import com.yarosh.library.user.repository.entity.UserEntity;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.Optional;
 
-public class UserRepository implements FindByNameRepository<UserEntity> {
+public interface UserRepository<E> {
 
-    private static final String SQL_SELECT_BY_USERNAME = """
-        SELECT u.id, u.username, u.firstname, u.lastname, u.password, r.name FROM users as u
-        JOIN roles as r on JSON_CONTAINS(roles, CAST(r.id as JSON))
-        """;
-
-    private final DataSource dataSource;
-
-    public UserRepository(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
-
-    @Override
-    public Optional<UserEntity> findByName(String name) {
-        try (Connection connection = dataSource.getConnection()){
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
+    Optional<E> findByName(String name);
 }
