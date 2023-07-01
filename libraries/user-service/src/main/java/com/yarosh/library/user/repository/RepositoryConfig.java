d807dbd5f20451d980e6b2a1b65b3e6056679ed6
Andrey Yarosh <andreyarosh7@gmail.com>
2023-07-01 15:07:13 +0300

    feature: added @Import(ConnectionPoolConfig.class)

--- libraries/user-service/src/main/java/com/yarosh/library/user/repository/RepositoryConfig.java
+++ libraries/user-service/src/main/java/com/yarosh/library/user/repository/RepositoryConfig.java
@@ -1,12 +1,15 @@
 package com.yarosh.library.user.repository;
 
+import com.yarosh.library.repository.pool.ConnectionPoolConfig;
 import com.yarosh.library.user.repository.entity.UserEntity;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 import javax.sql.DataSource;
 
 @Configuration
+@Import(ConnectionPoolConfig.class)
 public class RepositoryConfig {
 
     @Bean
