e8d7a366f25338a868909cad389dc111c1ff5246
Andrey Yarosh <andreyarosh7@gmail.com>
2023-07-01 15:09:01 +0300

    feature: added @ComponentScan("com.yarosh.library.user.repository")

--- libraries/user-service/src/main/java/com/yarosh/library/user/UserServiceConfig.java
+++ libraries/user-service/src/main/java/com/yarosh/library/user/UserServiceConfig.java
@@ -1,13 +1,17 @@
-package com.yarosh.library.user.service;
+package com.yarosh.library.user;
 
 import com.yarosh.library.user.domain.User;
 import com.yarosh.library.user.repository.UserRepository;
 import com.yarosh.library.user.repository.entity.UserEntity;
+import com.yarosh.library.user.service.UserService;
+import com.yarosh.library.user.service.UserServiceImpl;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ServiceConfig {
+@ComponentScan("com.yarosh.library.user.repository")
+public class UserServiceConfig {
 
     @Bean
     public UserService<User> findUserByUserNameService(UserRepository<UserEntity> findByNameRepository) {
