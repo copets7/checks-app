a88ad79066352ac3b7e8d0e27a9ade0611fcca0d
Andrey Yarosh <andreyarosh7@gmail.com>
2023-07-01 15:05:21 +0300

    feature: added jwtTokenService, userDetailsService Beans, removed configure method and added annotations @ComponentScan, @Import, @Order

--- libraries/authentication/jwt/src/main/java/com/yarosh/library/authentication/jwt/JwtSecurityConfig.java
+++ libraries/authentication/jwt/src/main/java/com/yarosh/library/authentication/jwt/JwtSecurityConfig.java
@@ -1,21 +1,25 @@
 package com.yarosh.library.authentication.jwt;
 
 import com.yarosh.library.authentication.jwt.service.JwtTokenService;
+import com.yarosh.library.authentication.jwt.service.JwtUserDetailsService;
+import com.yarosh.library.user.UserServiceConfig;
+import com.yarosh.library.user.domain.User;
+import com.yarosh.library.user.service.UserService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
+@ComponentScan("com.yarosh.library.authentication.jwt.service")
+@Import(UserServiceConfig.class)
+@Order(1)
 public class JwtSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    private final JwtTokenService jwtTokenService;
-
-    public JwtSecurityConfig(JwtTokenService jwtTokenService) {
-        this.jwtTokenService = jwtTokenService;
-    }
 
     @Bean
     @Override
@@ -23,22 +27,26 @@ public class JwtSecurityConfig extends WebSecurityConfigurerAdapter {
         return super.authenticationManagerBean();
     }
 
-    @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.httpBasic().disable()
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeRequests()
-                .antMatchers("/api/v1.0/login").permitAll()
-                .antMatchers("/api/v1.0/checks").hasAnyRole("ADMIN", "CASHIER")
-                .anyRequest().authenticated()
-                .and()
-                .apply(new JwtConfigurerAdapter(jwtTokenFilter()));
+    @Bean
+    public JwtConfigurerAdapter jwtConfigurerAdapter(JwtTokenFilter jwtTokenFilter) {
+        return new JwtConfigurerAdapter(jwtTokenFilter);
     }
 
     @Bean
-    public JwtTokenFilter jwtTokenFilter() {
+    public JwtTokenFilter jwtTokenFilter(JwtTokenService jwtTokenService) {
         return new JwtTokenFilter(jwtTokenService);
+    }
+
+    @Bean
+    public JwtTokenService jwtTokenService(@Value("${jwt.token.secret}") String secret,
+                                           @Value("${jwt.token.validity}") String validityInMilliSeconds,
+                                           UserDetailsService userDetailsService,
+                                           UserService<User> userService) {
+        return new JwtTokenService(secret, Long.parseLong(validityInMilliSeconds), userDetailsService, userService);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserService<User> userService) {
+        return new JwtUserDetailsService(userService);
     }
 }
