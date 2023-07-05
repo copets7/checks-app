29c8f6916780f89e155b9d8004cb1e55886c8f3b
Andrey Yarosh <andreyarosh7@gmail.com>
2023-07-05 19:21:25 +0300

    ref: added constructor and method validate

--- libraries/authentication/jwt/src/main/java/com/yarosh/library/authentication/jwt/domain/JwtResponse.java
+++ libraries/authentication/jwt/src/main/java/com/yarosh/library/authentication/jwt/domain/JwtResponse.java
@@ -1,6 +1,15 @@
 package com.yarosh.library.authentication.jwt.domain;
 
+import org.apache.commons.lang3.StringUtils;
+
 public record JwtResponse(String username, String token) {
+
+    public JwtResponse(String username, String token) {
+        this.username = username;
+        this.token = token;
+
+        validate();
+    }
 
     @Override
     public String toString() {
@@ -8,5 +17,14 @@ public record JwtResponse(String username, String token) {
                 "username='" + username + '\'' +
                 ", token = ********" + '\'' +
                 '}';
+    }
+
+    private void validate() {
+        if (StringUtils.isBlank(username)) {
+            throw new InvalidJwtUserException("Username is empty, {0}", this);
+        }
+        if (StringUtils.isBlank(token)) {
+            throw new InvalidJwtUserException("Token is empty, {0}", this);
+        }
     }
 }
