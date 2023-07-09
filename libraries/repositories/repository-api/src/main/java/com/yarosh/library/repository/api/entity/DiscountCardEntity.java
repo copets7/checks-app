26ddc72e8057be5c05ee54e408349c51cba96132
Andrey Yarosh <andreyarosh7@gmail.com>
2023-07-09 19:41:55 +0300

    ref: renamed Entity interface to BaseEntity and added javax persistence annotations

--- libraries/repositories/repository-api/src/main/java/com/yarosh/library/repository/api/entity/DiscountCardEntity.java
+++ libraries/repositories/repository-api/src/main/java/com/yarosh/library/repository/api/entity/DiscountCardEntity.java
@@ -1,8 +1,22 @@
 package com.yarosh.library.repository.api.entity;
 
-public class DiscountCardEntity implements Entity {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "discount_cards")
+public class DiscountCardEntity implements BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "discount")
     private double discount;
 
     public DiscountCardEntity() {
