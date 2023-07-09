756327b3f11092485139c359b3171595cf278856
Andrey Yarosh <andreyarosh7@gmail.com>
2023-07-09 19:41:38 +0300

    ref: renamed Entity interface to BaseEntity and added javax persistence annotations

--- libraries/repositories/repository-api/src/main/java/com/yarosh/library/repository/api/entity/CheckEntity.java
+++ libraries/repositories/repository-api/src/main/java/com/yarosh/library/repository/api/entity/CheckEntity.java
@@ -1,18 +1,50 @@
 package com.yarosh.library.repository.api.entity;
 
+import com.yarosh.library.repository.api.ProductsColumnConverter;
+
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Map;
 
-public class CheckEntity implements Entity {
+@Entity
+@Table(name = "checks")
+public class CheckEntity implements BaseEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "market_name")
     private String marketName;
+
+    @Column(name = "cashier_name")
     private String cashierName;
+
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "time")
     private LocalTime time;
+
+    @Convert(converter = ProductsColumnConverter.class)
     private Map<ProductEntity, Integer> products;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "discount_card_id")
     private DiscountCardEntity discountCard;
+
+    @Column(name = "total_price")
     private double totalPrice;
 
     public CheckEntity() {
