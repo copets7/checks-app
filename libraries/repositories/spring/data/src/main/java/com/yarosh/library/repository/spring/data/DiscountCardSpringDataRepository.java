c3d14f06318be10a3ac7f75432cb9d7169016597
Andrey Yarosh <andreyarosh7@gmail.com>
2023-07-09 19:44:47 +0300

    feature: added DiscountCardSpringDataRepository.java

---
+++ libraries/repositories/spring/data/src/main/java/com/yarosh/library/repository/spring/data/DiscountCardSpringDataRepository.java
@@ -0,0 +1,8 @@
+package com.yarosh.library.repository.spring.data;
+
+import com.yarosh.library.repository.api.entity.DiscountCardEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DiscountCardSpringDataRepository extends JpaRepository<DiscountCardEntity, Long> { }
