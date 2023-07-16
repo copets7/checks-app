5a0cc6dc57c4cae2eec6d07800da5cae4116866c
Andrey Yarosh <andreyarosh7@gmail.com>
2023-07-16 17:46:38 +0300

    ref: refactored DiscountCardRepositoryDecorator to SpringDataRepositoryDecorator.java

---
+++ libraries/repositories/spring/data/src/main/java/com/yarosh/library/repository/spring/data/decorator/SpringDataRepositoryDecorator.java
@@ -0,0 +1,42 @@
+package com.yarosh.library.repository.spring.data.decorator;
+
+import com.yarosh.library.repository.api.CrudRepository;
+import com.yarosh.library.repository.api.entity.BaseEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class SpringDataRepositoryDecorator<E extends BaseEntity, ID> implements CrudRepository<E, ID> {
+
+    private final JpaRepository<E, ID> jpaRepository;
+
+    public SpringDataRepositoryDecorator(JpaRepository<E, ID> jpaRepository) {
+        this.jpaRepository = jpaRepository;
+    }
+
+    @Override
+    public E insert(E entity) {
+        return jpaRepository.save(entity);
+    }
+
+    @Override
+    public Optional<E> select(ID id) {
+        return jpaRepository.findById(id);
+    }
+
+    @Override
+    public List<E> selectAll() {
+        return jpaRepository.findAll();
+    }
+
+    @Override
+    public E update(E entity) {
+        return jpaRepository.save(entity);
+    }
+
+    @Override
+    public void delete(ID id) {
+        jpaRepository.deleteById(id);
+    }
+}
