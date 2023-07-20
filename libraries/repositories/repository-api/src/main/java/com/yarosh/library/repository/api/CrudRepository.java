6856d6c7910e651eeba1a4a511dfc311eb616b46
Andrey Yarosh <andreyarosh7@gmail.com>
2023-07-20 23:54:43 +0300

    feature: added findAllWithPagination method

--- libraries/repositories/repository-api/src/main/java/com/yarosh/library/repository/api/CrudRepository.java
+++ libraries/repositories/repository-api/src/main/java/com/yarosh/library/repository/api/CrudRepository.java
@@ -1,6 +1,8 @@
 package com.yarosh.library.repository.api;
 
 import com.yarosh.library.repository.api.entity.BaseEntity;
+import com.yarosh.library.repository.api.pagination.DatabasePage;
+import com.yarosh.library.repository.api.pagination.DatabasePageRequest;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,4 +18,6 @@ public interface CrudRepository<E extends BaseEntity, ID> {
     E update(E entity);
 
     void delete(ID id);
+
+    DatabasePage<E> findAllWithPagination(final DatabasePageRequest request);
 }
