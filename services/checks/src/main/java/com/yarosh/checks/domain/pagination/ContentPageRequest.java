24eca6ac735c4b7dc8b3b130b29c2e2bfe6a2185
Andrey Yarosh <andreyarosh7@gmail.com>
2023-07-24 23:52:16 +0300

    feature: added toString method

--- services/checks/src/main/java/com/yarosh/checks/domain/pagination/ContentPageRequest.java
+++ services/checks/src/main/java/com/yarosh/checks/domain/pagination/ContentPageRequest.java
@@ -1,4 +1,13 @@
 package com.yarosh.checks.domain.pagination;
 
-public class ContentPageRequest {
+public record ContentPageRequest(Integer pageNumber, Integer size, ContentSortBy sortBy) {
+
+    @Override
+    public String toString() {
+        return "ContentPageRequest{" +
+                "pageNumber=" + pageNumber +
+                ", size=" + size +
+                ", sortBy=" + sortBy +
+                '}';
+    }
 }
