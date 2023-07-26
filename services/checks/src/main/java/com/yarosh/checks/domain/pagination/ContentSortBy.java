abc11a9c97b7dd2c822ac61180c4ed8c21885ca8
Andrey Yarosh <andreyarosh7@gmail.com>
2023-07-26 19:30:30 +0300

    feature: added constanta DEFAULT_SORTING_COLUMN and constructors

--- services/checks/src/main/java/com/yarosh/checks/domain/pagination/ContentSortBy.java
+++ services/checks/src/main/java/com/yarosh/checks/domain/pagination/ContentSortBy.java
@@ -2,11 +2,21 @@ package com.yarosh.checks.domain.pagination;
 
 public class ContentSortBy {
 
+    private static final String DEFAULT_SORTING_COLUMN = "id";
+
     private final String column;
     private final boolean isDesc;
 
     public ContentSortBy() {
-        this("id", true);
+        this(DEFAULT_SORTING_COLUMN, true);
+    }
+
+    public ContentSortBy(String column) {
+        this(column, true);
+    }
+
+    public ContentSortBy(boolean isDesc) {
+        this(DEFAULT_SORTING_COLUMN, isDesc);
     }
 
     public ContentSortBy(String column, boolean isDesc) {
