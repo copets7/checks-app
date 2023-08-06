c47e2c8ef8b7b74c823613a1f171c9e38e31ea58
Andrey Yarosh <andreyarosh7@gmail.com>
2023-08-06 17:20:27 +0300

    feature: added WebSecurityConfig.java

---
+++ services/checks/src/main/java/com/yarosh/checks/controller/config/WebSecurityConfig.java
@@ -0,0 +1,80 @@
+package com.yarosh.checks.controller.config;
+
+import com.yarosh.library.authentication.jwt.JwtSecurityConfig;
+import com.yarosh.library.authentication.jwt.JwtTokenFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@Import(JwtSecurityConfig.class)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String ROLE_PREFIX = "";
+    private static final String[] ROLES = { "ADMIN", "CASHIER" };
+    private static final String[] AUTH_WHITE_LIST = {
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            "/v2/api-docs/**",
+            "/swagger-resources/**",
+            "/api/v1.0/auth/login"
+    };
+    private static final String[] AUTH_RESOURCES = {
+            "/api/v1.0/check/**",
+            "/api/v1.0/product/**",
+            "/api/v1.0/discount-card/**"
+    };
+
+    private final JwtTokenFilter jwtTokenFilter;
+    private final UserDetailsService userDetailsService;
+
+    public WebSecurityConfig(JwtTokenFilter jwtTokenFilter, UserDetailsService userDetailsService) {
+        this.jwtTokenFilter = jwtTokenFilter;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(AUTH_WHITE_LIST).permitAll()
+                .antMatchers(AUTH_RESOURCES).hasAnyRole(ROLES)
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public GrantedAuthorityDefaults grantedAuthorityDefaults() {
+        return new GrantedAuthorityDefaults(ROLE_PREFIX);
+    }
+}
