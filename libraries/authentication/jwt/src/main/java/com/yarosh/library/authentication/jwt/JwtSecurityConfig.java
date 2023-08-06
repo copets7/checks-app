0cef44a5df1036331939d73dfe9f05733bb7b9cb
Andrey Yarosh <andreyarosh7@gmail.com>
2023-08-06 17:19:09 +0300

    ref: removed configure, passwordEncoder and jwtConfigurerAdapter methods

--- libraries/authentication/jwt/src/main/java/com/yarosh/library/authentication/jwt/JwtSecurityConfig.java
+++ libraries/authentication/jwt/src/main/java/com/yarosh/library/authentication/jwt/JwtSecurityConfig.java
@@ -11,38 +11,13 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @ComponentScan("com.yarosh.library.authentication.jwt.service")
 @Import(UserServiceConfig.class)
-@Order(1)
-public class JwtSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    private final UserDetailsService userDetailsService;
-
-    public JwtSecurityConfig(UserService<User> userService) {
-        this.userDetailsService = new JwtUserDetailsService(userService);
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public JwtConfigurerAdapter jwtConfigurerAdapter(JwtTokenFilter jwtTokenFilter) {
-        return new JwtConfigurerAdapter(jwtTokenFilter);
-    }
+public class JwtSecurityConfig {
 
     @Bean
     public JwtTokenFilter jwtTokenFilter(JwtTokenService jwtTokenService) {
@@ -50,11 +25,12 @@ public class JwtSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public JwtTokenService jwtTokenService(@Value("${jwt.token.secret}") String secret,
+    public JwtTokenService jwtTokenService(AuthenticationManager authenticationManager,
+                                           @Value("${jwt.token.secret}") String secret,
                                            @Value("${jwt.token.validity}") String validityInMilliSeconds,
                                            UserDetailsService userDetailsService,
                                            UserService<User> userService) {
-        return new JwtTokenService(secret, Long.parseLong(validityInMilliSeconds), userDetailsService, userService);
+        return new JwtTokenService(authenticationManager, secret, Long.parseLong(validityInMilliSeconds), userDetailsService, userService);
     }
 
     @Bean
