e046a7166dbe2c7346911b58c755db824f264b37
Andrey Yarosh <andreyarosh7@gmail.com>
2023-08-08 21:17:43 +0300

    ref: removed imports

--- libraries/authentication/jwt/src/main/java/com/yarosh/library/authentication/jwt/JwtSecurityConfig.java
+++ libraries/authentication/jwt/src/main/java/com/yarosh/library/authentication/jwt/JwtSecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
