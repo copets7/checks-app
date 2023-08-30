9cf573ee4e3f3b209038459ece04e5e5d934c4de
Andrey Yarosh <andreyarosh7@gmail.com>
2023-08-30 22:20:33 +0300

    feature: added CheckRecord.java

---
+++ libraries/reports-api/src/main/java/com/yarosh/library/reports/api/CheckRecord.java
@@ -0,0 +1,28 @@
+package com.yarosh.library.reports.api;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Collection;
+import java.util.Optional;
+
+public record CheckRecord(String marketName,
+                          String cashierName,
+                          LocalDate date,
+                          LocalTime time,
+                          Collection<String> products,
+                          Optional<Double> discount,
+                          double totalPrice) implements Record {
+
+    @Override
+    public String toString() {
+        return "CheckRecord{" +
+                "marketName='" + marketName + '\'' +
+                ", cashierName='" + cashierName + '\'' +
+                ", date=" + date +
+                ", time=" + time +
+                ", products=" + products +
+                ", discount=" + discount +
+                ", totalPrice=" + totalPrice +
+                '}';
+    }
+}
