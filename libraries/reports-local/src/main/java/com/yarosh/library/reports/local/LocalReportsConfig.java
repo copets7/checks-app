85e0b70b3529cce4b8d7eae23d399695bcd5006b
Andrey Yarosh <andreyarosh7@gmail.com>
2023-09-04 21:21:21 +0300

    feature: refactored checkReportsService Bean

--- libraries/reports-local/src/main/java/com/yarosh/library/reports/local/LocalReportsConfig.java
+++ libraries/reports-local/src/main/java/com/yarosh/library/reports/local/LocalReportsConfig.java
@@ -1,14 +1,16 @@
 package com.yarosh.library.reports.local;
 
+import com.yarosh.library.reports.api.CheckRecord;
+import com.yarosh.library.reports.api.ReportService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ReportsConfig {
+public class LocalReportsConfig {
 
     @Bean
-    public CheckReportsService checkReportsService(@Value("${reports.directory}")String reportsDir) {
+    public ReportService<CheckRecord> checkReportsService(@Value("${reports.directory}")String reportsDir) {
         return new CheckReportsService(reportsDir);
     }
 }
