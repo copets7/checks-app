1138c1950f45e469464a5808fb3d9e38cefc36b7
Andrey Yarosh <andreyarosh7@gmail.com>
2023-09-06 21:10:32 +0300

    feature: added methods createReportPath, performReportsDir and refactored method storeReport

--- libraries/reports-local/src/main/java/com/yarosh/library/reports/local/CheckReportsService.java
+++ libraries/reports-local/src/main/java/com/yarosh/library/reports/local/CheckReportsService.java
@@ -6,7 +6,12 @@ import com.yarosh.library.reports.api.ReportService;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.MessageFormat;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class CheckReportsService implements ReportService<CheckRecord> {
@@ -14,16 +19,22 @@ public class CheckReportsService implements ReportService<CheckRecord> {
     private static final String[] CSV_HEADER = {"Market", "Cashier", "Date", "Time", "Products", "Discount", "Total price"};
     private static final String NO_DISCOUNT = "";
     private static final int CSV_HEADER_INDEX = 0;
+    private static final String REPORTS_DIR_PATTERN = "{0}/{1}";
 
     private final String reportsDir;
 
-    public CheckReportsService(String reportDir) {
-        this.reportsDir = reportDir;
+    public CheckReportsService(String reportsDir) {
+        this.reportsDir = performReportsDir(reportsDir);
     }
 
     @Override
     public void storeReport(List<CheckRecord> records) {
-        try (CSVWriter writer = new CSVWriter(new FileWriter(reportsDir))) {
+        if (records.isEmpty()) {
+            return;
+        }
+
+        final String reportPath = createReportPath(records);
+        try (CSVWriter writer = new CSVWriter(new FileWriter(reportPath))) {
             final List<String[]> content = convertToCsvFormat(records);
             writer.writeAll(content);
         } catch (IOException e) {
@@ -47,5 +58,19 @@ public class CheckReportsService implements ReportService<CheckRecord> {
                 record.discount().map(Object::toString).orElse(NO_DISCOUNT),
                 String.valueOf(record.totalPrice())
         };
+    }
+
+    private String createReportPath(List<CheckRecord> records) {
+        return MessageFormat.format("{0}/{1}", reportsDir, records.get(0).date());
+    }
+
+    private String performReportsDir(String reportsDir) {
+        final Path target = Paths.get(Objects.requireNonNull(this.getClass().getResource("/")).getPath());
+        final Path reports = Paths.get(MessageFormat.format(REPORTS_DIR_PATTERN, target.toAbsolutePath(), reportsDir));
+        try {
+            return Files.createDirectories(reports).toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
