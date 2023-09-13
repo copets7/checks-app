3bf9eae0418b1d1ab6875d489992b692333abe35
Andrey Yarosh <andreyarosh7@gmail.com>
2023-09-13 21:23:51 +0300

    feature : changed Collection<ProductInfo> products to List<ProductInfo> products

--- libraries/reports/report-api/src/main/java/com/yarosh/library/reports/api/CheckRecord.java
+++ libraries/reports/report-api/src/main/java/com/yarosh/library/reports/api/CheckRecord.java
@@ -2,14 +2,14 @@ package com.yarosh.library.reports.api;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public record CheckRecord(String marketName,
                           String cashierName,
                           LocalDate date,
                           LocalTime time,
-                          Collection<ProductInfo> products,
+                          List<ProductInfo> products,
                           Optional<Double> discount,
                           double totalPrice) implements Record {
 
