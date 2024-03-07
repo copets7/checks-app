8c5b4e09577a5203a9d99da1d53b94331d1f1488
copets7 <andreyarosh7@gmail.com>
2024-03-08 00:31:44 +0300

    ref: optimized imports

--- services/checks/src/main/java/com/yarosh/checks/service/CheckService.java
+++ services/checks/src/main/java/com/yarosh/checks/service/CheckService.java
@@ -18,7 +18,6 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import javax.inject.Inject;
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
